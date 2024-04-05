194699e9179bf6af02548360b0fba42804cad1d5
camichaves79 <camilo.chaves.h@gmail.com>
2024-04-05 11:50:26 -0500

    revise

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function check(){
+function check() {
     let word = document.getElementById("text-input").value;
-    if (word == ""){
+    if (word == "") {
         alert("Please input a value");
         return null
     };
     let alphanum = "";
-    for(let i = 0; i < word.length; i++){
-        if (word[i].match(/[a-zA-Z0-9]/)){
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].match(/[a-zA-Z0-9]/)) {
             alphanum += word[i]
-        }; 
+        };
     };
     alphanum = alphanum.toUpperCase()
-    let strRev =  alphanum.split('').reverse().join('');
-    if (alphanum == strRev){
+    let strRev = alphanum.split('').reverse().join('');
+    if (alphanum == strRev) {
         document.getElementById("result").innerText = word += ' is a palindrome'
-    } 
-    else{
+    }
+    else {
         document.getElementById("result").innerText = word += ' is not a palindrome'
     }
     ;
